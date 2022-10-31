d73505f8403018b234157d04e09ba8426ec9b85f
mattiamorosi <66827085+mattiamorosi@users.noreply.github.com>
2022-10-31 09:50:20 +0100

    commit

--- createIdentity.js
+++ createIdentity.js
@@ -1,6 +1,5 @@
 const { IdentityManager, Types } = require("@tanglelabs/identity-manager");
 const path = require("path");
-const asyncHandler = require("express-async-handler");
 
 const createIdentity = async (alias) => {
     const manager = await IdentityManager.newInstance({
--- index.js
+++ index.js
@@ -32,29 +32,25 @@ app.use(
 
 app.post("/createIdentity", async function(req,res) {
     const { alias } = req.body.data;
-    console.log("Post request received, alias: "+alias)
+    console.log("POST /createIdentity received, alias: "+alias)
     const did = await createIdentity(alias);
-    console.log("DID: "+JSON.stringify(did));
     if (did==="alias_used") res.send("alias_used");
     else res.json(did);
 })
 
 app.post("/getDDO", async function(req,res) {
   const { alias } = req.body.data;
-  console.log("Post request received, alias: "+alias)
+  console.log("POST /getDDO received, alias: "+alias)
   const ddo = await getDDO(alias);
-  console.log("DDO: "+JSON.stringify(ddo));
   if (ddo==="Not_found") res.send("Not_found");
   else res.json(ddo);
 })
 
 
 app.post("/createAP", async function(req,res) {
-  console.log("Qui arriva");
   const { alias } = req.body.data;
-  console.log("POST request received on /createAP, alias: "+alias)
+  console.log("POST /createAP received, alias: "+alias)
   const did = await createAP(alias);
-  console.log("DID: "+JSON.stringify(did));
   if (did==="alias_used") res.send("alias_used");
   else res.json(did);
 })
@@ -64,7 +60,6 @@ app.post("/createUserVC", async function(req,res) {
   const { name, surname, university, userAlias } = req.body.data;
   console.log("Params: "+name+", "+surname+", "+university+", "+userAlias);
   const vc = await createUserVc(name, surname, university, userAlias);
-  console.log("VC: "+JSON.stringify(vc));
   res.json(vc);
 })
 
